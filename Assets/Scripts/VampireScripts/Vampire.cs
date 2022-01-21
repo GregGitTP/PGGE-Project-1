6e01a93c40d7c179cc1b908611e247677b50920e
Greg <2000247C@student.tp.edu.sg>
2022-01-21 23:02:40 +0800

    Setup basic movement for vampire, need to redownload walking anim

--- Assets/Scripts/VampireScripts/Vampire.cs
+++ Assets/Scripts/VampireScripts/Vampire.cs
@@ -6,11 +6,34 @@ using PGGE;
 
 public class Vampire : MonoBehaviour
 {
+    [Header("Component references")]
+    public Transform camera;
+    public Transform player;
+    public CharacterController cc;
+    public Animator anim;
+
+    [Header("Values for character movement")]
+    [Space(10)]
+    public float movementSpeed = 2f;
+    public float rotationSpeed = 240f;
+    public float runMultiplier = 2f;
+    public float gravity = -20f;
+    public float jumpForce = 8f;
+
+    [Header("The X, Z and Z axis offsets for the third person camera")]
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
     FSM fsm = new FSM();
     State moveState, attackState, reloadState;
 
     private void Start(){
+        moveState = new VampireMovementState(fsm, camera, player, this, cc, anim, movementSpeed, rotationSpeed, runMultiplier, gravity, jumpForce, xOffset, yOffset, zOffset);
 
+        fsm.Add(0, moveState);
+
+        fsm.SetCurrentState(moveState);
     }
 
     private void Update(){
--- Assets/Scripts/VampireScripts/VampireMovementState.cs
+++ Assets/Scripts/VampireScripts/VampireMovementState.cs
@@ -6,15 +6,109 @@ using PGGE;
 
 public class VampireMovementState : State
 {
-    public VampireMovementState(FSM _fsm) : base(_fsm){}
+    float movementSpeed, rotationSpeed, runMultiplier, gravity, jumpForce, x, y, z;
 
-    public override void Enter(){}
+    Transform camera, player;
+    CharacterController cc;
+    Animator anim;
+    MonoBehaviour mb;
 
-    public override void Exit(){}
+    TPCFollowTrackRotation tpc;
+    RepositionCamera rc;
 
-    public override void Update(){}
+    Vector3 velocity = Vector3.zero;
+    bool running = false;
+    bool jump = false;
+    bool crouch = false;
 
-    public override void FixedUpdate(){}
+    public VampireMovementState(FSM _fsm, Transform _camera, Transform _player, MonoBehaviour _mb, CharacterController _cc, Animator _anim, float _movementSpeed, float _rotationSpeed, float _runMultiplier, float _gravity, float _jumpForce, float _x, float _y, float _z) : base(_fsm){
+        camera = _camera;
+        player = _player;
+        mb = _mb;
+        cc = _cc;
+        anim = _anim;
+        movementSpeed = _movementSpeed;
+        rotationSpeed = _rotationSpeed;
+        runMultiplier = _runMultiplier;
+        gravity = _gravity;
+        jumpForce = _jumpForce;
+        x = _x;
+        y = _y;
+        z = _z;
+    }
 
-    public override void LateUpdate(){}
+    public override void Enter(){
+        tpc = new TPCFollowTrackRotation(camera, player, x, y, z);
+        rc = new RepositionCamera(camera, player, tpc);
+    }
+
+    public override void Exit(){
+        tpc = null;
+        rc = null;
+    }
+
+    public override void Update(){
+        HandleInputs();
+        Move();
+    }
+
+    public override void FixedUpdate(){
+        ApplyGravity();
+    }
+
+    public override void LateUpdate(){
+        tpc.Update();
+        rc.Update();
+    }
+
+    private void HandleInputs(){
+        if(Input.GetKeyDown(KeyCode.LeftShift)) running = true;
+        
+        if(Input.GetKeyUp(KeyCode.LeftShift)) running = false;
+
+        if(Input.GetKeyDown(KeyCode.Space)) jump = true;
+
+        if(Input.GetKeyUp(KeyCode.Space)) jump = false;
+
+        if(Input.GetKeyDown(KeyCode.Tab)){
+            crouch = !crouch;
+            Crouch();
+        }  
+    }
+
+    private void Move(){
+        float vert = Input.GetAxis("Vertical");
+        float hori = Input.GetAxis("Horizontal");
+
+        if (running && !(vert < 0)) vert *= runMultiplier;
+
+        Vector3 move = player.forward * vert * movementSpeed;
+
+        cc.Move(move*Time.deltaTime);
+
+        player.Rotate(0f, hori*rotationSpeed*.3f*Time.deltaTime, 0f);
+
+        anim.SetFloat("PosX", hori/2);
+        anim.SetFloat("PosY", vert/2);
+
+        if(jump && anim.GetCurrentAnimatorStateInfo(0).IsName("GroundMovement")) mb.StartCoroutine(Jump()); 
+    }
+
+    private void ApplyGravity(){
+        velocity.y += gravity * Time.deltaTime;
+
+        cc.Move(velocity * Time.deltaTime);
+
+        if(cc.isGrounded && velocity.y < 0f) velocity.y = 0f;
+    }
+
+    public IEnumerator Jump(){
+        anim.SetTrigger("Jump");
+        yield return new WaitForSeconds(.5f);
+        velocity.y += jumpForce;
+    }
+
+    private void Crouch(){
+        anim.SetBool("Crouch", crouch);
+    }
 }
